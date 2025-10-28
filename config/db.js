ddfd31f3d0cf5a95571f0b9a970c3c2e6a7f4ce0
Mohamed Ghanem <mg0447837@gmail.com>
2025-10-28 18:35:37 +0300

    prepare backend for vercel

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URI);
+
   console.log("Database connected successfully");
 };
 
