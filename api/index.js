e7077797c84bf5cdd6070927ea4ba3b10ac74e8d
Mohamed Ghanem <mg0447837@gmail.com>
2025-10-29 17:41:48 +0300

    Vercel setup

---
+++ api/index.js
@@ -0,0 +1,28 @@
+import serverless from "serverless-http";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "../config/db.js";
+import User from "../routes/user.js";
+import Doctor from "../routes/doctor.js";
+import Appointment from "../routes/appointment.js";
+import Departments from "../routes/Departments.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/user", User);
+app.use("/doctors", Doctor);
+app.use("/appointments", Appointment);
+app.use("/departments", Departments);
+
+app.get("/", (req, res) => {
+  res.send("Doctor Appointment Backend on Vercel ✅");
+});
+
+export const handler = serverless(app);
